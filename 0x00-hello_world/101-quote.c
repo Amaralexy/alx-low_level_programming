13863e8b373cf04950fbebefde43bdfeec091cca
Amaralexy <amarachiokei3@gmail.com>
2022-07-24 04:42:26 +0000

    Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ int main(void)
 {
 		char qt[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-			fprintf(stderr, "%s\n", qt);
+			printf(stderr, "%s\n", qt);
 				return (1);
 }
