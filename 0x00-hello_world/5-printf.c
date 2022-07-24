1e4c214bdf96febb3ebf565174b3ded2a7e2d51e
Amaralexy <amarachiokei3@gmail.com>
2022-07-24 03:21:40 +0000

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - this is the strating point of the program
- * Return: returns 0
- */
+* main - This is a piece of art
+* Return: returns 0
+*/
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
